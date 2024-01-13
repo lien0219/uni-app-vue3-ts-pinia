8916bdca403fa7a92f0a6f415a8e454753af07d5
lien0219 <lien0001@163.com>
2024-01-13 17:52:37 +0800

    feat:完成前台分类模块

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,18 +1,26 @@
 <script setup lang="ts">
 import { onLoad } from '@dcloudio/uni-app'
 import CustomNavbar from './components/CustomNavbar.vue'
-import { getHomeBannerAPI } from '@/services/home'
+import CategoryPanel from './components/CategoryPanel.vue'
+import { getHomeBannerAPI, getHomeCategoryAPI } from '@/services/home'
 import { ref } from 'vue'
-import type { BannerItem } from '@/types/home'
+import type { BannerItem, CategoryItem } from '@/types/home'
 
+// 轮播图
 const bannerList = ref<BannerItem[]>([])
 const getHomeBannerData = async () => {
   const res = await getHomeBannerAPI()
   bannerList.value = res.result
 }
-
+// 分类
+const categoryList = ref<CategoryItem[]>([])
+const getHomeCategoryData = async () => {
+  const res = await getHomeCategoryAPI()
+  categoryList.value = res.result
+}
 onLoad(() => {
   getHomeBannerData()
+  getHomeCategoryData()
 })
 </script>
 
@@ -21,9 +29,13 @@ onLoad(() => {
   <CustomNavbar></CustomNavbar>
   <!-- 自定义轮播组件 -->
   <XtxSwiper :list="bannerList"></XtxSwiper>
+  <!-- 分类 -->
+  <CategoryPanel :list="categoryList"></CategoryPanel>
   <view class="index">index</view>
 </template>
 
 <style lang="scss">
-//
+page {
+  background-color: #f7f7f7;
+}
 </style>
